18908051aeebc3af22c6c4284f61929fd623c5ae
james <jamesagmiles@gmail.com>
2020-11-28 18:14:39 +0000

    Added DDL for activity and pins tables, added folder for transformations

---
+++ rdbms/tables/activity.sql
@@ -0,0 +1,6 @@
+CREATE TABLE main.activity
+(
+	pin_id INTEGER,
+	reading NUMERIC,
+	measured_at TIMESTAMP WITHOUT TIME ZONE
+);
---
+++ rdbms/tables/pins.sql
@@ -0,0 +1,6 @@
+CREATE TABLE main.pins
+(
+	pin_id INTEGER,
+	x NUMERIC,
+	y NUMERIC
+);
