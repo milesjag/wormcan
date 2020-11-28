3b0bc503b18f56ff8e06a340281b5b8cb0ea5c99
james <jamesagmiles@gmail.com>
2020-11-28 19:22:00 +0000

    Added simple interfaces for returning results set from touchpad sensor readings

--- test_touch.py
+++ test_touch.py
@@ -3,6 +3,8 @@ import board
 import busio
 import adafruit_mpr121
 import logging
+import datetime
+from typing import Dict, List, Union
 
 def get_logger():
     logging.basicConfig(level=logging.DEBUG)
@@ -12,21 +14,48 @@ def get_logger():
 
 log = get_logger()
 
-def run_touch(touchpad):
-    touch_count = 0
-    num_sensors= [1]
-    log.debug(f"Number of sensors set to {len(num_sensors)}")
-    log.debug("Running touch sensors...")
-    while True:
-        for i in num_sensors:
-            touch_count += 1
-            log.debug(f"Input {i} - Baseline: {touchpad.baseline_data(i)} Filtered: {touchpad.filtered_data(i)}, Contact: {touchpad.is_touched(i)}")
+def measure_pin_activity(touchpad, pin_id) -> Dict[str, Union[int, float]]:
+    """
+    Measure electical activity data for the supplied pin ID and
+    return results object
+    """
+    return {
+            "pin_id": pin_id,
+            "baseline": touchpad.baseline_data(pin_id),
+            "filtered": touchpad.filtered_data(pin_id),
+            "is_touched": touchpad.is_touched(pin_id)
+            }
 
+def measure_touchpad_activity(touchpad):
+    """
+    Measure electical activity data for all active pins
+    and return results object
+    """
+    measured_dt = datetime.datetime.now(datetime.timezone.utc)
+    active_sensors= range(12)
+    results_set = []
+    log.debug(f"Number of active sensors: {len(active_sensors)}")
+    log.debug("Running touchpad activity measurements...")
+    for pin_id in active_sensors:
+        results_set.append(
+                {
+                    "measured_at": measured_dt,
+                    **measure_pin_activity(touchpad, pin_id)
+                }
+        )
+    log.debug("Completed touchpad activity measurements") 
+    return results_set
+    
 
-if __name__ == "__main__":
+def initialize_touchpad() -> adafruit_mpr121.MPR121:
     log.debug("Imports successful")
     i2c = busio.I2C(board.SCL, board.SDA)
     log.debug("I2C setup successful")
     mpr121 = adafruit_mpr121.MPR121(i2c)
     log.debug("MPR121 setup successful")
-    run_touch(mpr121)
+    return mpr121
+
+if __name__ == "__main__":
+    touchpad = initialize_touchpad()
+    results = measure_touchpad_activity(touchpad)
+    print(results)
